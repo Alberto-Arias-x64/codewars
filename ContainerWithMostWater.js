694ad883414c4a4955716ff9c1f9b5bef61c86c1
Alberto Arias <alberto.arias.x64@gmail.com>
2023-09-05 15:22:58 -0500

    ContainerWithMostWater

--- ContainerWithMostWater.js
+++ ContainerWithMostWater.js
@@ -1,5 +1,20 @@
-function ContainerWithMostWater(list){
+function ContainerWithMostWater(list) {
+    let result = 0
+    let counter1 = 0
+    let counter2 = list.length - 1
 
+    const calculateWater = (height1, height2, distance) => {
+        if (height1 < height2) return height1 * distance
+        return height2 * distance
+    }
+
+    for (let index = 0; index < list.length; index++) {
+        if (result < calculateWater(list[counter1], list[counter2], counter2 - counter1)) result = calculateWater(list[counter1], list[counter2], counter2 - counter1)
+        if (list[counter1] < list[counter2]) counter1++
+        else counter2++
+    }
+
+    return result
 }
 
-console.log(ContainerWithMostWater([1,8,6,2,5,4,8,3,7]))
+console.log(ContainerWithMostWater([1, 8, 6, 2, 5, 4, 8, 3, 7]))
