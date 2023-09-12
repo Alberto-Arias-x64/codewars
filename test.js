e256b53af02be462466106db504c7e870a05987a
Albertox64 <alberto.arias.x64@gmail.com>
2023-09-11 21:14:50 -0500

    (update)

--- test.js
+++ test.js
@@ -4,3 +4,18 @@ import path from "path"
 fs.ReadStream(path.join(__dirname, "",))
 
 console.log(__dirname)
+
+const readMore = 0
+
+readMore = 1
+
+//this si a error
+
+const unaVida = () => {
+    console.log("nepe")
+    console.log("atoro")
+}
+
+function Vpoer(no, tiene) {
+    return no + tiene
+}
