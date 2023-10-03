c783681ec537bd91eb10d9fc6fa2398c4ffc5713
Albertox64 <alberto.arias.x64@gmail.com>
2023-10-02 21:58:37 -0500

    searchInMatrix

--- Search2dArray.js
+++ Search2dArray.js
@@ -1,26 +1,26 @@
-function searchInMatrix(matrix = [], objetivo) {
-    const horizontal = matrix[0].length
-    const vertical = matrix.length
+function searchInmatriz(matriz = [], objetivo) {
+    const horizontal = matriz[0].length
+    const vertical = matriz.length
     let dy = 0
     let dx = 0
     while (true) {
-        if (matrix[dy][0] <= objetivo && matrix[dy][horizontal - 1] >= objetivo) break
+        if (matriz[dy][0] <= objetivo && matriz[dy][horizontal - 1] >= objetivo) break
         else if (dy !== vertical) dy++
         else return false
     }
     while (true) {
-        if (matrix[dy][dx] <= objetivo && matrix[dy][dx] >= objetivo) break
+        if (matriz[dy][dx] <= objetivo && matriz[dy][dx] >= objetivo) break
         else if (dx !== horizontal) dx++
         else return false
     }
-    return [dy, dx]
+    return true
 }
 
-const matrix = [
+const matriz = [
     [1, 3, 5, 7],
     [10, 11, 16, 20],
     [23, 30, 34, 60],
 ]
 
-console.log(searchInMatrix(matrix, 3))
-console.log(searchInMatrix(matrix, 16))
+console.log(searchInmatriz(matriz, 3))
+console.log(searchInmatriz(matriz, 16))
---
+++ Search2dArray.py
@@ -0,0 +1,33 @@
+def searchInMatrix(matriz, objetivo):
+  horizontal = len(matriz[0])
+  vertical = len(matriz)
+  dy = 0
+  dx = 0
+  while True:
+      if matriz[dy-1][0] <= objetivo and matriz[dy-1][horizontal - 1] >= objetivo:
+          break
+      elif dy != vertical:
+          dy += 1
+      else:
+          return False
+  while True:
+      if matriz[dy-1][dx -1] <= objetivo and matriz[dy-1][dx -1] >= objetivo:
+          break
+      elif dx != horizontal:
+          dx += 1
+      else:
+          return False
+  return True
+
+matriz = [
+  [1,3,5,7],
+  [10,11,16,20],
+  [23,30,34,60],
+]
+print(matriz)
+
+response = searchInMatrix(matriz, 3)
+print(response)
+
+response2 = searchInMatrix(matriz, 61)
+print(response2)
