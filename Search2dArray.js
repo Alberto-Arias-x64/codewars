b389977e9e6304c84f771c7d5d0025bc1400174a
Albertox64 <alberto.arias.x64@gmail.com>
2023-10-02 21:48:22 -0500

    searchInMatrix

--- Search2dArray.js
+++ Search2dArray.js
@@ -1,12 +1,26 @@
-function searchInMatrix(matriz, objetivo){
-    return false
+function searchInMatrix(matrix = [], objetivo) {
+    const horizontal = matrix[0].length
+    const vertical = matrix.length
+    let dy = 0
+    let dx = 0
+    while (true) {
+        if (matrix[dy][0] <= objetivo && matrix[dy][horizontal - 1] >= objetivo) break
+        else if (dy !== vertical) dy++
+        else return false
+    }
+    while (true) {
+        if (matrix[dy][dx] <= objetivo && matrix[dy][dx] >= objetivo) break
+        else if (dx !== horizontal) dx++
+        else return false
+    }
+    return [dy, dx]
 }
 
-const matriz = [
-  [1,3,5,7],
-  [10,11,16,20],
-  [23,30,34,60],
+const matrix = [
+    [1, 3, 5, 7],
+    [10, 11, 16, 20],
+    [23, 30, 34, 60],
 ]
 
-console.log(searchInMatrix(matriz, 3))
-console.log(searchInMatrix(matriz, 12))
+console.log(searchInMatrix(matrix, 3))
+console.log(searchInMatrix(matrix, 16))
