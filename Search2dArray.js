251ce7a23544d5d3d9b88783c88cd9326c2652c9
Albertox64 <alberto.arias.x64@gmail.com>
2023-10-02 21:32:06 -0500

    Search2dArray

---
+++ Search2dArray.js
@@ -0,0 +1,12 @@
+function searchInMatrix(matriz, objetivo){
+    return false
+}
+
+const matriz = [
+  [1,3,5,7],
+  [10,11,16,20],
+  [23,30,34,60],
+]
+
+console.log(searchInMatrix(matriz, 3))
+console.log(searchInMatrix(matriz, 12))
