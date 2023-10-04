8873273ee144ac02c21ab7b8d472a42f60a6da2f
Alberto Arias <alberto.arias.x64@gmail.com>
2023-10-04 17:09:46 -0500

    permutations

---
+++ SoManyPermutations.js
@@ -0,0 +1,41 @@
+// In this kata, your task is to create all permutations of a non-empty input string and remove duplicates, if present.
+
+// Create as many "shufflings" as you can!
+
+// Examples:
+
+// With input 'a':
+// Your function should return: ['a']
+
+// With input 'ab':
+// Your function should return ['ab', 'ba']
+
+// With input 'abc':
+// Your function should return ['abc','acb','bac','bca','cab','cba']
+
+// With input 'aabb':
+// Your function should return ['aabb', 'abab', 'abba', 'baab', 'baba', 'bbaa']
+// Note: The order of the permutations doesn't matter.
+
+// Good luck!
+
+function permutations(string = '') {
+    if (string.length === 1) {
+        return [string];
+    }
+    const map = []
+    for (var i = 0; i < string.length; i++) {
+        let letter = string[i];
+        let rest = string.slice(0, i) + string.slice(i + 1);
+        let permutationsRest = permutations(rest);
+
+        for (var j = 0; j < permutationsRest.length; j++) {
+            map.push(letter + permutationsRest[j]);
+        }
+    }
+    const response = new Set()
+    map.forEach(element => response.add(element))
+    return response
+}
+
+console.log(permutations('abc'))
