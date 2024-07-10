e0206ddfd8498003e20be0a964e4e82cd6c7a5a2
Alberto Arias <alberto.arias.x64@gmail.com>
2024-07-09 21:39:18 -0500

    catch car

---
+++ CatchingCarMileageNumbers.js
@@ -0,0 +1,126 @@
+// "7777...8?!??!", exclaimed Bob, "I missed it again! Argh!" Every time there's an interesting number coming up,
+// he notices and then promptly forgets. Who doesn't like catching those one-off interesting mileage numbers?
+
+// Let's make it so Bob never misses another interesting number. We've hacked into his car's computer,
+// and we have a box hooked up that reads mileage numbers.
+// We've got a box glued to his dash that lights up yellow or green depending on whether it receives a 1 or a 2 (respectively).
+
+// It's up to you, intrepid warrior, to glue the parts together.
+// Write the function that parses the mileage number input, and returns a 2 if the number is "interesting" (see below),
+// a 1 if an interesting number occurs within the next two miles, or a 0 if the number is not interesting.
+
+// Note: In Haskell, we use No, Almost and Yes instead of 0, 1 and 2.
+
+// "Interesting" Numbers
+// Interesting numbers are 3-or-more digit numbers that meet one or more of the following criteria:
+
+// Any digit followed by all zeros: 100, 90000
+// Every digit is the same number: 1111
+// The digits are sequential, incementing†: 1234
+// The digits are sequential, decrementing‡: 4321
+// The digits are a palindrome: 1221 or 73837
+// The digits match one of the values in the awesomePhrases array
+// † For incrementing sequences, 0 should come after 9, and not before 1, as in 7890.
+// ‡ For decrementing sequences, 0 should come after 1, and not before 9, as in 3210.
+
+// So, you should expect these inputs and outputs:
+
+// // "boring" numbers
+// isInteresting(3, [1337, 256]);    // 0
+// isInteresting(3236, [1337, 256]); // 0
+
+// // progress as we near an "interesting" number
+// isInteresting(11207, []); // 0
+// isInteresting(11208, []); // 0
+// isInteresting(11209, []); // 1
+// isInteresting(11210, []); // 1
+// isInteresting(11211, []); // 2
+
+// // nearing a provided "awesome phrase"
+// isInteresting(1335, [1337, 256]); // 1
+// isInteresting(1336, [1337, 256]); // 1
+// isInteresting(1337, [1337, 256]); // 2
+// Error Checking
+// A number is only interesting if it is greater than 99!
+// Input will always be an integer greater than 0, and less than 1,000,000,000.
+// The awesomePhrases array will always be provided, and will always be an array, but may be empty.
+// (Not everyone thinks numbers spell funny words...)
+// You should only ever output 0, 1, or 2.
+
+// return 2 if is a interesting number
+// return 1 if next a interesting number of 2 milles
+// return 0 if is a bored number
+
+function upSequence(number = 0) {
+  if (number < 100) return false
+  const string = number.toString().split('')
+  for (let index = 0; index < string.length - 1; index++) {
+    if (Number(string[index]) + 1 !== Number(string[index + 1] == '0' ? '10' : string[index + 1])) return false
+  }
+  return true
+}
+function downSequence(number = 0) {
+  if (number < 100) return false
+  const string = number.toString().split('')
+  for (let index = 0; index < string.length - 1; index++) {
+    if (Number(string[index]) - 1 !== Number(string[index + 1])) return false
+  }
+  return true
+}
+function checkPalindrome(number = 0) {
+  if (number < 100) return false
+  const forward = number.toString()
+  const backward = number.toString().split('').reverse().join('')
+  return forward === backward
+}
+function checkRounded(number = 0) {
+  const rest = number.toString().split('')
+  rest.shift()
+  const zeros = rest.filter((e) => e == '0')
+  return zeros.length === rest.length
+}
+function isInteresting(number, awesomePhrases) {
+  if (number < 98) return 0
+  if (number > 1000000000) return 0
+  if (awesomePhrases.includes(number)) return 2
+  if (checkPalindrome(number)) return 2
+  if (upSequence(number)) return 2
+  if (downSequence(number)) return 2
+  if (checkRounded(number)) return 2
+  ////////////////////////////////////////////////////////////////////////////
+  if (awesomePhrases.includes(number + 1)) return 1
+  if (checkPalindrome(number + 1)) return 1
+  if (upSequence(number + 1)) return 1
+  if (downSequence(number + 1)) return 1
+  if (checkRounded(number + 1)) return 1
+  ////////////////////////////////////////////////////////////////////////////
+  if (awesomePhrases.includes(number + 2)) return 1
+  if (checkPalindrome(number + 2)) return 1
+  if (upSequence(number + 2)) return 1
+  if (downSequence(number + 2)) return 1
+  if (checkRounded(number + 2)) return 1
+  return 0
+}
+
+// console.log(isInteresting(3, [1337, 256]), 0)
+//console.log(isInteresting(123456789, [1337, 256]), 2)
+//console.log(isInteresting(987654321, [1337, 256]), 2)
+console.log(isInteresting(98, []), 1)
+// console.log(isInteresting(1336, [1337, 256]), 1)
+// console.log(isInteresting(1337, [1337, 256]), 2)
+// console.log(isInteresting(11208, [1337, 256]), 0)
+// console.log(isInteresting(11209, [1337, 256]), 1)
+// console.log(isInteresting(11211, [1337, 256]), 2)
+
+
+// Second Taka XD
+function sumStrings(a, b) {
+  return (BigInt(a) + BigInt(b)).toString()
+}
+
+console.log(sumStrings('123456789', '987654321'), '1111111110')
+console.log(sumStrings('712569312664357328695151392', '8100824045303269669937'), '712577413488402631964821329')
+
+
+console.log(parseFloat('0.201') + parseFloat('0.101'))
+console.log(0.30000000000000004)
