e45d2e42f02ed5fec4c2fe0fae21537cd1103826
Alberto Arias <alberto.arias.x64@gmail.com>
2024-07-21 20:51:47 -0500

    boyaca

--- DecodeTheMorseCode.js
+++ DecodeTheMorseCode.js
@@ -33,7 +33,7 @@
 
 // After you complete this kata, you may try yourself at Decode the Morse code, advanced.
 
-// Adding a comments for lucky guy un regalo
+// Adding a comments for lucky guy un regalo bogota santamaria boyaca
 
 
 const MORSE_CODE = {
