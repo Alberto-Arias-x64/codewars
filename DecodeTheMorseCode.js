3ec66595a8500b5379a9f6b9edf7e84892821364
Alberto Arias <alberto.arias.x64@gmail.com>
2024-07-23 20:26:56 -0500

    hubspot

--- DecodeTheMorseCode.js
+++ DecodeTheMorseCode.js
@@ -33,7 +33,7 @@
 
 // After you complete this kata, you may try yourself at Decode the Morse code, advanced.
 
-// Adding a comments for lucky guy un regalo bogota santamaria boyaca muelto
+// Adding a comments for lucky guy un regalo bogota santamaria boyaca muelto hubspot es una mierda
 
 
 const MORSE_CODE = {
